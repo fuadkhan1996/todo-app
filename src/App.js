4ebecf91832452769d41f6e4fc29c3ab3c375b3c
fuadkhan1996 <fuad.khan@thedevden.co>
2020-03-03 13:04:21 +0500

    Add folder for Header in components directory

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import TodoList from './Components/TodoList';
-import Header from './Components/Header';
+import Header from './Components/Header/';
 import { Container } from 'react-bootstrap';
 
 function App() {
